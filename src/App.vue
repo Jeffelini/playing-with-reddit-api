df4b87991abbdd092d1013108a2cdafaed46e0c0
Jeffelini <35406352+Jeffelini@users.noreply.github.com>
2018-09-08 21:56:45 -0400

    Linked client to Reddit API

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <h1>Reddit Client</h1>
     <div class="post-list">
       <div v-for="(p, index) in posts" :key="p.id" class="post-wrapper">
         <h3>Post {{ index + 1 }}:</h3>
@@ -10,16 +11,22 @@
 </template>
 
 <script>
-import posts from "./mock.json";
+import mockPosts from "./mock.json";
 import Post from "@/components/Post.vue";
+import { getPosts } from "@/services/reddit.service.js";
 
 export default {
   name: "app",
   data: function() {
-    return { posts };
+    return { posts: mockPosts };
   },
   components: {
     Post
+  },
+  mounted: function() {
+    getPosts().then(newPosts => {
+      this.posts = newPosts;
+    });
   }
 };
 </script>
@@ -43,12 +50,18 @@ body {
   flex-direction: column;
   align-items: center;
 
-  padding-top: 50px;
   font-family: "Pragati Narrow";
 
   .post-wrapper {
     width: 65%;
     min-width: 300px;
   }
+}
+
+h1 {
+  font-family: "Pragati Narrow";
+  text-align: center;
+  font-size: 100px;
+  margin-bottom: 0px;
 }
 </style>
